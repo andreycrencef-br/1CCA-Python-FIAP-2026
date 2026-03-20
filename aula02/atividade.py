b67e5a59beb3317f1ce8e095c21b8460295a0337
andrey <andreycrencef@gmail.com>
2026-03-20 11:31:40 -0300

    Aula 02 - Variáveis e Operadores + Desafio/Atividade

---
+++ aula02/atividade.py
@@ -0,0 +1,10 @@
+nome = str(input("Qual seu nome? "))
+print("Seja bem vindo ", nome)
+
+dia = int(input("Qual o dia do seu aniversario? "))
+mes = int(input("Certo. e o mês? "))
+ano = int(input("E por fim, o ano? "))
+print(f"Você nasceu em: {dia}/{mes}/{ano}")
+
+idade = int(input("Qual sua idade? "))
+print("Então ano que vem você terá:", idade + 1)
---
+++ aula02/aula02.py
@@ -0,0 +1,19 @@
+print("Olá mundo")
+
+print(7 + 6)
+print("7 + 4")
+
+#VARIAVEIS
+nome = "Andrey" #str
+peso = 1.1 #float
+idade = 20 #int
+
+print(nome, idade, peso)
+
+
+#INPUT - SIMULAÇÃO DE FORMS DO CMD
+nome = input("Digia o nome ai: ")
+print ("Olá", nome)
+idade = int(input("Qual sua idade?"))
+print("Olá", nome, ", Aparentemente você tem", idade, "anos", ". Ano que vem você fará", idade + 1, "anos")
+
